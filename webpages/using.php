041bd72a49d4cb25880aadced8cd510647b20b4d
kylev <kylev@981c7233-bbb0-e711-95a9-001517a2e1a4>
2006-09-20 00:55:51 +0000

    standard cleanup continues
    git-svn-id: https://svn.code.sf.net/p/lame/svn/branches/web-2006@5439 981c7233-bbb0-e711-95a9-001517a2e1a4

--- webpages/using.php
+++ webpages/using.php
@@ -1,57 +1,59 @@
-<!DOCTYPE html
-     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
      "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
 <html>
 <head>
-	<title>LAME MP3 Encoder</title>
-	<meta name="author" content="Roberto Amorim - roberto@rjamorim.com" />
-	<meta name="generator" content="jEdit 4.2" />
-	<meta http-equiv="Content-Style-Type" content="text/css" />
-	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-	<link rel="stylesheet" type="text/css" href="./styles/lame.css" />
-    <!--[if IE]>
-      <link rel="stylesheet" type="text/css" href="./styles/ie.css" />
-    <![endif]-->
+  <title>LAME MP3 Encoder :: Using LAME</title>
+  <meta name="author" content="Roberto Amorim - roberto@rjamorim.com" />
+  <meta name="generator" content="jEdit 4.2" />
+  <meta name="cvs-version" content="$Id: using.php,v 1.1.2.2 2006-09-20 00:55:51 kylev Exp $" />
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+  <link rel="stylesheet" type="text/css" href="styles/lame.css" />
 </head>
 <body>
-	<?php include("menu.html") ?>
-	<div id="container">
-		<div id="content">
-			<div align="center"><img src="./images/logo.gif" width="358" height="231" alt="LAME Official logo"></img>
-			<h1>Using LAME</h1></div>
-				<ul>
-					<li>
-						<a href="download.php">Download the source code (CVS, .tar and source RPMS 
-						available).</a>
-					</li>
-					<li>
-						<a href="links.php">Software which uses LAME</a>
-					</li>
-					<li>
-						<a href="links.php">MP3 Related Links</a> 
-					</li>
-					<li>
-						<a href="license.txt">Can I use LAME in non-open source software?</a>
-					</li>
-					<li>
-						<a href="lame_ui_example.php">User interface guidelines</a>, if you wish to use LAME in 
-						your program.
-					</li>
-				</ul>
-			<h3>Documentation:</h3>
-				<ul>
-					<li>
-						<a href="USAGE">Guide to command line options</a>
-					</li>
-					<li>
-						<a href="tech-FAQ.txt">Technical FAQ</a>
-					</li>
-					<li>
-						<a href="doc/html/index.html">Program documentation (LAME 3.96)</a>
-					</li>
-				</ul>
-		</div>
-	<?php include("footer.html") ?>
-	</div>
+
+<?php include("menu.html") ?>
+
+<div id="container">
+<div id="content">
+
+<div align="center">
+  <img src="images/logo.gif" width="358" height="231" alt="LAME Official Logo" />
+  <h1>Using LAME</h1>
+</div>
+
+<ul>
+
+  <li><a href="download.php">Download the source code (CVS, .tar and source
+  RPMS available).</a></li>
+
+  <li><a href="links.php">Software which uses LAME</a></li>
+
+  <li><a href="links.php">MP3 Related Links</a></li>
+
+  <li><a href="license.txt">Can I use LAME in non-open source
+  software?</a></li>
+
+  <li><a href="lame_ui_example.php">User interface guidelines</a>, if you wish
+  to use LAME in your program.</li>
+
+</ul>
+
+<h3>Documentation:</h3>
+
+<ul>
+
+  <li><a href="USAGE">Guide to command line options</a></li>
+
+  <li><a href="tech-FAQ.txt">Technical FAQ</a></li>
+
+  <li><a href="doc/html/index.html">Program documentation (LAME 3.96)</a></li>
+
+</ul>
+
+</div>
+<?php include("footer.html") ?>
+</div>
+
 </body>
 </html>
