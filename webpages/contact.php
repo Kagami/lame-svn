3702fd69673b297046521e911541bbb487328c82
kylev <kylev@981c7233-bbb0-e711-95a9-001517a2e1a4>
2006-09-23 19:06:34 +0000

    add metion of IRC, remove extra whitespace
    git-svn-id: https://svn.code.sf.net/p/lame/svn/trunk@5472 981c7233-bbb0-e711-95a9-001517a2e1a4

--- webpages/contact.php
+++ webpages/contact.php
@@ -6,7 +6,7 @@
   <title>LAME MP3 Encoder :: Contact Information</title>
   <meta name="author" content="Roberto Amorim - roberto@rjamorim.com" />
   <meta name="generator" content="jEdit 4.2" />
-  <meta name="cvs-version" content="$Id: contact.php,v 1.2 2006-09-23 18:44:36 kylev Exp $" />
+  <meta name="cvs-version" content="$Id: contact.php,v 1.3 2006-09-23 19:06:34 kylev Exp $" />
   <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
   <link rel="stylesheet" type="text/css" href="styles/lame.css" />
   <!--[if IE]>
@@ -24,13 +24,13 @@
   <img src="images/logo.gif" width="358" height="231" alt="LAME Official Logo" />
   <h1>Contact Information</h1>
 </div>
-			
+
 <h3>MP3 encoder mailing list</h3>
-			
+
 <p>For general discussions and comments about LAME and MP3 encoding, you can
 check the <a href="list.php">MP3 encoder mailing list</a>. Keep in mind this is
 <em>not</em> the preferred list if you want to get in touch with the developers.</p>
-                        
+
 <p><a
 href="http://www.mail-archive.com/mp3encoder%40minnie.tuhs.org/">Archives</a>
 are also available (older archives:<a
@@ -38,39 +38,45 @@ href="http://www.mail-archive.com/mp3encoder%40minnie.cs.adfa.edu.au/"> October
 2000 - Sept 2001</a> and <a
 href="http://www.mail-archive.com/mp3encoder%40geek.rcc.se">before October
 2000</a>)</p>
-			
+
 <h3>LAME developement mailing list</h3>
-			
+
 <p><a href="http://lists.sourceforge.net/lists/listinfo/lame-dev">The lame-dev
 list</a> is right place for everything related to LAME development: bug reports,
 questions and suggestions concerning the encoder, the documentation and the web
 pages. It's also the best way to get in touch with the active developers. <a
 href="http://sourceforge.net/mailarchive/forum.php?forum_id=5500">Archives</a>
 are provided by SourceForge.</p>
-			
+
 <h3>CVS mailing list</h3>
-			
+
 <p>If you want to be automatically notified of commits to CVS, you can subscribe
 to <a href="http://lists.sourceforge.net/lists/listinfo/lame-cvs">the lame-cvs
 list</a>.</p>
-			
+
+<h3>IRC</h3>
+
+<p>Some developers and other interested parties can periodically be found on the
+#mp3encoder channel on the <a href="http://freenode.net/">FreeNode
+Network</a>.</p>
+
 <h3>Bug reports/tracker</h3>
-			
+
 <p>To submit bug reports, you can use the <a
 href="http://sourceforge.net/tracker/?group_id=290&amp;atid=100290">bug
 tracker</a> besides the <a
 href="http://lists.sourceforge.net/lists/listinfo/lame-dev">lame-dev mailing
 list</a>. Please be descriptive in your bug report. You should clearly mention
 your problem, LAME version used and how to reproduce the bug.</p>
-			
+
 <h3>Contact developers</h3>
-			
+
 <p>Here is the list of <a href="developers.php">developers</a>. If you encounter
 a problem, please use any of the ways mentioned above instead of directly
 contact a developer.</p>
-			
+
 <h3>Contact the Webmaster</h3>
-			
+
 <p>The current Webmaster for the LAME project is <a
 href="http://www.rjamorim.com">Roberto Amorim</a>. You can contact him through
 <a href="mailto:rjamorim@users.sourceforge.net">e-mail</a>.</p>
