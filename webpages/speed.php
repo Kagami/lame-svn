184685338123fe63f457015ba94afc8f88bbfb34
kylev <kylev@981c7233-bbb0-e711-95a9-001517a2e1a4>
2006-09-23 18:39:47 +0000

    More templating/cleanup
    git-svn-id: https://svn.code.sf.net/p/lame/svn/branches/web-2006@5469 981c7233-bbb0-e711-95a9-001517a2e1a4

--- webpages/speed.php
+++ webpages/speed.php
@@ -1,74 +1,79 @@
-<!DOCTYPE html
-     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
      "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
 <html>
 <head>
-	<title>LAME MP3 Encoder</title>
-	<meta name="author" content="Roberto Amorim - roberto@rjamorim.com" />
-	<meta name="generator" content="jEdit 4.2" />
-	<meta http-equiv="Content-Style-Type" content="text/css" />
-	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-	<link rel="stylesheet" type="text/css" href="./styles/lame.css" />
-    <!--[if IE]>
-      <link rel="stylesheet" type="text/css" href="./styles/ie.css" />
-    <![endif]-->
+  <title>LAME MP3 Encoder :: Speed Benchmarks</title>
+  <meta name="author" content="Roberto Amorim - roberto@rjamorim.com" />
+  <meta name="generator" content="jEdit 4.2" />
+  <meta name="cvs-version" content="$Id: speed.php,v 1.1.2.2 2006-09-23 18:39:47 kylev Exp $" />
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+  <link rel="stylesheet" type="text/css" href="styles/lame.css" />
+  <!--[if IE]>
+  <link rel="stylesheet" type="text/css" href="styles/ie.css" />
+  <![endif]-->
 </head>
 <body>
-	<?php include("menu.html") ?>
-	<div id="container">
-		<div id="content">
-			<div align="center"><img src="./images/logo.gif" width="358" height="231" alt="LAME Official logo"></img>
-			<h1>Speed benchmarks</h1></div>
-			<p>
-				Here is a <a href="http://www.hispamp3.com/bench/index.htm">MP3 Speed Comparison</a> 
-				Web Site
-			</p>
-			<hr></hr>
-<pre>January 2000: 
-From: "Peter Olufsen"  - po@dsinet.dk 
 
-Processor:     600Mhz pentium III 
-Platform:      Win-98 
-WAV file:      262sec. song (45.2Mb). 
+<?php include("menu.html") ?>
 
- CBR, high quality (-h / qual 9), full freq. range (-k) 
+<div id="container">
+<div id="content">
 
-                   Mp3Enc31    BladeEnc     Lame3.60 
-192 (-ms)            586         166          60 
+<div align="center">
+  <img src="images/logo.gif" width="358" height="231" alt="LAME Official Logo" />
+  <h1>Speed Benchmarks</h1>
+</div>
+
+<pre>January 2000:
+From: "Peter Olufsen"  - po@dsinet.dk
+
+Processor:     600Mhz pentium III
+Platform:      Win-98
+WAV file:      262sec. song (45.2Mb).
+
+ CBR, high quality (-h / qual 9), full freq. range (-k)
+
+                   Mp3Enc31    BladeEnc     Lame3.60
+192 (-ms)            586         166          60
 128 (-mj)            795         182          90</pre>
-			
-			<hr></hr>
-<pre>November  1999: 
-From: "Ethan Yeo" - ewyeo@bigfoot.com 
 
-Processor:    AMD Athlon 650 
-Platform:     Microsoft Windows NT 4.0 SP6 
-WAV File:     Track 1 from "The Prince of Egypt" OST 
+<hr />
 
-Encoder          Timing*       Size 
------------------------------------------------ 
-Track1.wav       304 sec  53,712,668 Bytes 
-BladeEnc 0.82    153 sec   4,871,604 Bytes   bladeenc track1.wav track1.mp3 
-LAME 3.51         80 sec   4,872,440 Bytes   lame -h track1.wav track1.mp3 
-LAME 3.57beta     75 sec   4,872,022 Bytes   lame -h track1.wav track1.mp3 
+<pre>November  1999:
+From: "Ethan Yeo" - ewyeo@bigfoot.com
 
-LAME 3.51         70 sec   4,872,440 Bytes   lame -h -m s track1.wav track1.mp3 
+Processor:    AMD Athlon 650
+Platform:     Microsoft Windows NT 4.0 SP6
+WAV File:     Track 1 from "The Prince of Egypt" OST
+
+Encoder          Timing*       Size
+-----------------------------------------------
+Track1.wav       304 sec  53,712,668 Bytes
+BladeEnc 0.82    153 sec   4,871,604 Bytes   bladeenc track1.wav track1.mp3
+LAME 3.51         80 sec   4,872,440 Bytes   lame -h track1.wav track1.mp3
+LAME 3.57beta     75 sec   4,872,022 Bytes   lame -h track1.wav track1.mp3
+
+LAME 3.51         70 sec   4,872,440 Bytes   lame -h -m s track1.wav track1.mp3
 LAME 3.57beta     64 sec   4,872,022 Bytes   lame -h -m s track1.wav track1.mp3</pre>
 
-			<hr></hr>
-<pre>July 1999:  LAME 3.13 timings from  Alex Nemirovsky - alexn@transport.com 
-  
+<hr />
 
-WAV file:   284 seconds 
-  
+<pre>July 1999:  LAME 3.13 timings from  Alex Nemirovsky - alexn@transport.com
 
-266 Mhz Pentium, 66 Mhz FSB, 512 KB cache at 66Mhz   encoding time:       295 sec, 
-233 Mhz Pentium II/ 66Mhz FSB, 512 KB cache at 115Mhz encoding time:      234 sec, 
-200 Mhz Pentium Pro/66Mhz FSB, 256KB cache at 200Mhz encoding time:       257 sec, 
-300 Mhz Celeron 300A/66Mhz FSB, 128KB cache at 300Mhz encoding time:      180 sec, 
+
+WAV file:   284 seconds
+
+
+266 Mhz Pentium, 66 Mhz FSB, 512 KB cache at 66Mhz   encoding time:       295 sec,
+233 Mhz Pentium II/ 66Mhz FSB, 512 KB cache at 115Mhz encoding time:      234 sec,
+200 Mhz Pentium Pro/66Mhz FSB, 256KB cache at 200Mhz encoding time:       257 sec,
+300 Mhz Celeron 300A/66Mhz FSB, 128KB cache at 300Mhz encoding time:      180 sec,
 300 Mhz Pentium II/66Mhz FSB, 512 KB cache at 150Mhz, encoding time:      176 sec,</pre>
-		</div>
-	<?php include("footer.html") ?>
-	</div>
+
+</div>
+<?php include("footer.html") ?>
+</div>
+
 </body>
 </html>
