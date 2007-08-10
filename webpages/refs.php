1a477f9e0576d8f05fba370c0974aba3c308e086
rjamorim <rjamorim@981c7233-bbb0-e711-95a9-001517a2e1a4>
2007-08-10 00:05:18 +0000

    fixed some 404 links, removed others that have been irremediably lost.
    git-svn-id: https://svn.code.sf.net/p/lame/svn/trunk@5648 981c7233-bbb0-e711-95a9-001517a2e1a4

--- webpages/refs.php
+++ webpages/refs.php
@@ -6,7 +6,7 @@
 	<title>LAME MP3 Encoder :: References</title>
 	<meta name="author" content="Roberto Amorim - roberto@rjamorim.com" />
 	<meta name="generator" content="jEdit 4.2" />
-	<meta name="cvs-version" content="$Id: refs.php,v 1.3 2007-01-13 00:21:30 rjamorim Exp $" />
+	<meta name="cvs-version" content="$Id: refs.php,v 1.4 2007-08-10 00:05:18 rjamorim Exp $" />
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 	<link rel="stylesheet" type="text/css" href="styles/lame.css" />
 	<!--[if IE]>
@@ -36,7 +36,7 @@
 </p>
 
 <p>
-	<a href="http://www.eas.asu.edu/~Espeech/ndtc/">NDTC Speech and Audio Coding
+	<a href="http://www.eas.asu.edu/~speech/ndtc/">NDTC Speech and Audio Coding
 	Page</a> This page contains a detailed and very well written review article 
 	on psycho-acoustics.
 </p>
@@ -45,15 +45,12 @@
 	Davis Pan has a couple of usefull overviews of MPEG audio encoding:
 	<br /> 
 	<a	href="doc/vol5num2art3.pdf">vol5num2art3.pdf</a>
-	<br/>
-	<a href="http://www.bok.net/~pan">http://www.bok.net/~pan</a>
 </p>
 
 <p>
 	Here are some papers by Frank Baumgarte:
-	<br /> <a
-	href="http://www.tnt.uni-hannover.de/project/coding/audio/perception/publications.html">
-	http://www.tnt.uni-hannover.de/project/coding/audio/perception/publications.html</a>
+	<br /> <a href="http://www.tnt.uni-hannover.de/papers/view.php?ind=Baumgarte">
+	http://www.tnt.uni-hannover.de/papers/view.php?ind=Baumgarte</a>
 	<br/> The paper by Baumgarte, Ferekidis and Fuchs describes an alternative
 	psycho-acoustic model for MPEG encoding.
 </p>
@@ -63,12 +60,6 @@
 	project). The assocated paper gives some good information on audio encoding.
 	<br/> <a
 	href="http://www-ccrma.stanford.edu/~bosse/">http://www-ccrma.stanford.edu/~bosse/</a>
-</p>
-
-<p>
-	Here is some very detailed documentation on a parallel layer II encoder:<br/><a
-	href="http://www.ece.miami.edu/~fernando/html/thesis/thesis.html">
-	http://www.ece.miami.edu/~fernando/html/thesis/thesis.html</a>
 </p>
 
 <p>
